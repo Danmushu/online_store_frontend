adaf3d27c8ff0082f606f2d88bd9ef45559c51c0
Danmushu <Danmushu@outlook.com>
2024-11-26 21:57:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import CategoryPage from "@/components/categoryPage.vue";
+</script>
+
+<template>
+<!--  <header>-->
+<!--    <div class="wrapper">-->
+<!--    </div>-->
+<!--  </header>-->
+  <div class="app">
+    <CategoryPage />
+  </div>
+
+<!--  <main>-->
+<!--    -->
+<!--  </main>-->
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+/*
+//@media (min-width: 1024px) {
+//  header {
+//    display: flex;
+//    place-items: center;
+//    padding-right: calc(var(--section-gap) / 2);
+//  }
+//
+//  .logo {
+//    margin: 0 2rem 0 0;
+//  }
+//
+//  header .wrapper {
+//    display: flex;
+//    place-items: flex-start;
+//    flex-wrap: wrap;
+//  }
+//}
+*/
+
+</style>
